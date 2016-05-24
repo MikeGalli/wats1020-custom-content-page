cd1b7b568a0817cf634223795e74b1792abff952
MikeGalli <mikegalligar@Mikes-MacBook-Pro-2.local>
2016-05-23 19:48:51 -0700

    tg

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ $(document).on('ready', function(){
 
   var searchImages = function(tags) {
     var flickrAPI = "http://api.flickr.com/services/feeds/photos/141199663@N04/albums/72157668420144852.gne?jsoncallback=?";
-    https://www.flickr.com/photos/141199663@N04/albums/72157668420144852
     console.log(tags) + "tags";
     $('#images').innerHTML = '<li class="search-throbber">Searching...</li>';
     $.getJSON( flickrAPI, {
@@ -15,12 +14,12 @@ $(document).on('ready', function(){
       $('#images').empty();
       $('h1.search-title').first()[0].innerHTML = "Search for: " + tags;
       $.each( data.items, function( i, item ) {
-        var newListItem = $("<li>")
+        var newListItem = $("<li> .placeholder")
         // If you're not doing the modal, then show info about the image.
-        var newTitle = $('<p class="image-title">').html(item.title).appendTo(newListItem);
-        var newDate = $('<p class="image-date">').text(item.date_taken).appendTo(newListItem);
-        var newDescription = $('<p class="image-description">').html(item.description).appendTo(newListItem);
-        var newLink = $('<a>').attr('href', item.link).text('View on Flickr.').appendTo(newListItem);
+        //var newTitle = $('<p class="image-title">').html(item.title).appendTo(newListItem);
+        //var newDate = $('<p class="image-date">').text(item.date_taken).appendTo(newListItem);
+        //var newDescription = $('<p class="image-description">').html(item.description).appendTo(newListItem);
+        //var newLink = $('<a>').attr('href', item.link).text('View on Flickr.').appendTo(newListItem);
 
         newListItem.appendTo( "#images" );
         if ( i === 15 ) {
