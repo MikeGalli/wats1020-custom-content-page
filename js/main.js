5e88c0d9c07fa60bcc35d012aa37d511ffad6158
MikeGalli <mikegalligar@Mikes-MacBook-Pro-2.local>
2016-05-24 11:31:24 -0700

    xa

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).on('ready', function(){
   $('#button-spy').affix({
   });
-
+  $("catagories")
   var searchImages = function(tags) {
     var flickrAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
     //var flickrAPI = "http://api.flickr.com/services/feeds/photos/141199663@N04/albums/72157668420144852.gne?jsoncallback=?";
